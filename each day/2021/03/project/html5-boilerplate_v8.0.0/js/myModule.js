e237f6e1ccc576df4b05c826eaf5c24316fd0523
Fengsss <1048782143@qq.com>
2021-04-03 16:49:52 +0800

    3月
    3月

---
+++ each day/2021/03/project/html5-boilerplate_v8.0.0/js/myModule.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+  return a + b;
+}
+
+// 导出函数 add
+exports.add = add;
+console.log('module myModule:', module);
---
+++ each day/2021/03/project/html5-boilerplate_v8.0.0/js/nodeMain.js
@@ -0,0 +1,6 @@
+// main.js
+//const myModule = require('./myModule.js');
+const EventEmitter = require('events').EventEmitter;
+// 调用 myModule.js 中的 add 函数
+//results = myModule.add(1, 2);
+console.log('module main:', module);
